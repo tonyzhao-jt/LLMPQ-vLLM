b6d3312954114068e897a6abcaeddd45ce2fb326
zhaojuntao <zhaojuntao@bytedance.com>
2025-03-06 15:14:17 +0800

    determine the later PQ revision step

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ LLM-PQ v2
 ```bash
     export LD_LIBRARY_PATH=/opt/conda/envs/llmpq/lib/python3.10//site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
 ```
+
+# Implementation Milestones
+全包好了：对于任意模型
+(1)indicator gen：https://github.com/tonyzhao-jt/LLM-PQ/tree/main/scripts/accuracyPPL
+用 gen ind 生成 indicator, hess
+(2)profiler: https://github.com/tonyzhao-jt/LLM-PQ/tree/main/scripts/profile
+先用下面这个简单测算不同 GPU 下的 Latency （可以加一个 TP 的维度，默认用 TP=node）
+https://github.com/ModelCloud/GPTQModel?tab=readme-ov-file#dynamic-quantization-per-module-quantizeconfig-override
+打包成对应之前的 fit 格式. 通信还是用之前的测法 （可有可无了属于是）
+(4) 新的 cost model 结果可能要分析一下。包括 component 的时间。
+(3) optimizer
+用优化器解，解完后，得到计划直接用 vllm 跑 (dummy 就行了) (核心是看怎么 PP 了，quant 这边直接 load 就行了)
+(4)讨论一下 chunked prefill 啥的, 从效果上也是堆叠到上面
+PD 分离有点 contradict， KD 分离 本身增加了memory 使用，但 quant 一般永远在 memory scarse 的情况下
--- test/test_offline.py
+++ test/test_offline.py
@@ -12,10 +12,23 @@ prompts = [
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=100)
 
-# Create an LLM.
-# llm = LLM(model="facebook/opt-125m", cpu_offload_gb=10) # try cpu offload
-# llm = LLM(model="meta-llama/Llama-3.2-1B", load_format='dummy') # try dummy 
-llm = LLM(model="meta-llama/Llama-3.2-1B", cpu_offload_gb=10) # try cpu offload
+# 全包好了：对于任意模型
+# indicator gen：
+# 用 gen ind 生成 indicator, hess
+# profiler:
+# 先用下面这个简单测算不同 GPU 下的 Latency （可以加一个 TP 的维度，默认用 TP=node）
+# https://github.com/ModelCloud/GPTQModel?tab=readme-ov-file#dynamic-quantization-per-module-quantizeconfig-override
+# 打包成对应之前的 fit 格式
+# 通信还是用之前的测法
+# optimizer
+# 用优化器解，解完后，得到计划直接用 vllm 跑 (核心是看怎么 PP 了，quant 这边直接 load 就行了)
+# 这还涉及到 chunked prefill 啥的, 从效果上也是堆叠到上面
+# PD 分离有点 contradict， KD 分离 本身增加了memory 使用，但 quant 一般永在 memory scarse 的情况下
+llm = LLM(model="meta-llama/Llama-3.2-1B", cpu_offload_gb=10, load_format='dummy', quantization_config=q_config) # try cpu offload
+# 然后再用 vllm 直接跑就行了。。
+# indicator 的脚本还是可以用之前的。
+
+# 逻辑：用之前的脚本获取
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
