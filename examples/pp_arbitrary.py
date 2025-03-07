8b175637529892740177b0fe9b8a9217e4568b1a
zhaojuntao <zhaojuntao@bytedance.com>
2025-03-07 20:16:56 +0800

    [dev] validate the dynamic partition of PP on vllm

--- examples/pp_arbitrary.py
+++ examples/pp_arbitrary.py
@@ -1,15 +1,13 @@
-# run vllm in arbitrary partition 
+# run vllm in arbitrary partition
 # SPDX-License-Identifier: Apache-2.0
 # https://github.com/vllm-project/vllm/blob/main/tests/distributed/test_pipeline_partition.py
 # https://github.com/vllm-project/vllm/tree/main/tests/distributed
-from vllm import LLM, SamplingParams
+import os
 
-'''
-vllm serve gpt2 \
---tensor-parallel-size 4 \
---pipeline-parallel-size 2 \
---distributed_executor_backend ray
-'''
+from transformers import AutoConfig
+from vllm import SamplingParams
+
+from llmpq.optimizer import is_partition_config_valid
 
 # Sample prompts.
 prompts = [
@@ -21,17 +19,20 @@ prompts = [
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=100)
 
-# ray start --head 
-llm = LLM(model="meta-llama/Llama-3.2-1B", 
-          pipeline_parallel_size=4, 
-          load_format='dummy', 
-          enforce_eager=True, # try cpu offload
-          ) # try cpu offload
+# use autoconfig to get the layer number of the model
+model_id = "meta-llama/Llama-3.2-1B"
+config = AutoConfig.from_pretrained(model_id)
+num_layers = config.num_hidden_layers
+partition_config = "2,6,6,2"
+pipeline_parallel_size = 4
+is_partition_config_valid(
+    (partition_config, pipeline_parallel_size), num_layers
+)  # noqa
+os.environ["VLLM_PP_LAYER_PARTITION"] = partition_config
 
-outputs = llm.generate(prompts, sampling_params)
-# Print the outputs.
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    tkn_num = len(output.outputs[0].token_ids)
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}, token_num: {tkn_num}")
+"""
+    vllm serve meta-llama/Llama-3.2-1B \
+    --tensor-parallel-size 4 \
+    --pipeline-parallel-size 4 \
+    --load-format dummy
+"""
---
+++ llmpq/optimizer/__init__.py
@@ -0,0 +1,3 @@
+from .utils import is_partition_config_valid  # noqa
+
+all = ["is_partition_config_valid"]
