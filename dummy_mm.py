638479618db17df35dac71f234e0739fae684980
zhaojuntao <zhaojuntao@bytedance.com>
2025-04-20 23:48:00 +0800

    [dev] add dummy mm

---
+++ dummy_mm.py
@@ -0,0 +1,24 @@
+import torch
+import time
+
+nrank = torch.cuda.device_count()
+shape = (8192, 8192)
+lhs = [torch.randn(shape, device=f"cuda:{rank}") for rank in range(nrank)]
+rhs = [torch.randn(shape, device=f"cuda:{rank}") for rank in range(nrank)]
+
+while True:
+    for i in range(nrank):
+        o = lhs[i] @ rhs[i]
+    time.sleep(0.005)
+
+
+# expect rpdb==0.1.6
+def debug_rpdb():
+    import rpdb
+    import time
+    try:
+        rpdb.Rpdb(port=6678 + int(time.time()) % 6677).set_trace()
+    except Exception as ee:
+        print(f"luqi failed: {ee}")
+        while True:
+            time.sleep(1000)
