ca179ed43e8bf01575bb498f05f0767b23477670
zhaojuntao <zhaojuntao@bytedance.com>
2025-04-28 22:24:52 +0800

    [dev] update mot

--- examples/parse_trace.py
+++ examples/parse_trace.py
@@ -8,19 +8,21 @@ if __name__ == "__main__":
         'LogitsProcessor': 1
     }
     trace_paths = [
-        '/opt/tiger/Saber/llm_pq_v2/examples/tmp/vllm_profile_parsed/Llama_3.2_1B_Instruct_sharded-bitsandbytes-8-1-pt.trace.json',
-        # '/opt/tiger/Saber/llm_pq_v2/examples/tmp/vllm_profile_parsed/Llama_3.2_1B_Instruct_sharded-gptq-4-1-pt.trace.json',
-        # '/opt/tiger/Saber/llm_pq_v2/examples/tmp/vllm_profile_parsed/Llama_3.2_1B_Instruct_sharded-gptq-8-1-pt.trace.json'
+        '/opt/tiger/Saber/llm_pq_v2/examples/tmp/Tesla_V100-SXM2-32GB/vllm_profile_parsed/2/Qwen_Qwen2.5-32B-3-1-pt.trace.json',
+        '/opt/tiger/Saber/llm_pq_v2/examples/tmp/Tesla_V100-SXM2-32GB/vllm_profile_parsed/2/Qwen_Qwen2.5-32B-4-1-pt.trace.json',
+        '/opt/tiger/Saber/llm_pq_v2/examples/tmp/Tesla_V100-SXM2-32GB/vllm_profile_parsed/2/Qwen_Qwen2.5-32B-8-1-pt.trace.json',
+        '/opt/tiger/Saber/llm_pq_v2/examples/tmp/Tesla_V100-SXM2-32GB/vllm_profile_parsed/2/Qwen_Qwen2.5-32B-16-1-pt.trace.json',
     ]
     bits = [
-        '8-tc',
-        # '4',
-        # '8'
+        '3',
+        '4',
+        '8',
+        16
     ]
     cost_table_dict = {}
     for trace_path, bit in zip(trace_paths, bits):
         cost_table, gpu_name = parse_module_avg_cost(module_cnt, trace_path)
         cost_table_dict[bit] = cost_table
     save_with_pickle(cost_table_dict, 'cost_table_dict_tc.pkl', './')
-    print(cost_table)
+    print(cost_table_dict)
     print(gpu_name)
--- examples/read_profile.py
+++ examples/read_profile.py
@@ -1,4 +1,4 @@
-path = '/opt/tiger/Saber/llm_pq_v2/examples/cost_table_dict.pkl'
+path = '/opt/tiger/Saber/llm_pq_v2/examples/cost_table_dict_tc.pkl'
 import pickle
 with open(path, 'rb') as f:
     cost_table_dict = pickle.load(f)
